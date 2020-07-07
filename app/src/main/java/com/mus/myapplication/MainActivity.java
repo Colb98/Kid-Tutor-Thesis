fbb18e35007413bbf20de5affc2cc124c6b1ecc5
Loc Nguyen <1612343@student.hcmus.edu.vn>
2020-07-07 16:24:15 +0700

    fix status bar

--- app/src/main/java/com/mus/myapplication/MainActivity.java
+++ app/src/main/java/com/mus/myapplication/MainActivity.java
@@ -55,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                | View.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
 
         // Must have. Setting up everything
